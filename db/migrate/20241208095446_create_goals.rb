7bf0605baa12270cbe2e0f3e5e1a2c8a60e6941f
ryoh827 <rn.gjt25@gmail.com>
2024-12-08 19:06:02 +0900

    add migration and association

--- db/migrate/20241208095446_create_goals.rb
+++ db/migrate/20241208095446_create_goals.rb
@@ -1,6 +1,10 @@
 class CreateGoals < ActiveRecord::Migration[8.0]
   def change
     create_table :goals do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.datetime :due_date, null: false
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_08_092006) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_08_095446) do
+  create_table "goals", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.datetime "due_date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "goals", "users"
 end
